3be14b36bbd170ff9c2043a51d00706bdbf8e9c3
Ryan Bradley <bradleyrp@gmail.com>
2016-04-21 11:25:12 -0400

    fixed modules path error

--- gromacs.py
+++ gromacs.py
@@ -2,7 +2,7 @@
 machine_configuration = {
 	'LOCAL':dict(
 		nprocs = 4,
-		module_path = '/usr/share/modules/init/python.py',
+		module_path = '/usr/share/Modules/default/init/python.py',
 		modules = 'gromacs/gromacs-4.6.3',
 		),
 	}
