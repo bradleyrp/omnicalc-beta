fd3184a611e261a76594f9a120371134da317c3a
Ryan Bradley <bradleyrp@gmail.com>
2016-04-22 15:26:52 -0400

    updated gromacs.py from automacs

--- makefile
+++ makefile
@@ -3,19 +3,31 @@
 #-------------------------------------------------------------------------------------------------------------
 
 #---always show the banner
--include banner
 banner:
 	@echo -n "[STATUS] banner: "
 	@sed -n 1,7p omni/readme.md
 	@echo "[STATUS] use 'make help' for details"
 
-#---valid function names from the python script
-TARGETS := $(shell perl -n -e '@parts = /^def\s+[a-z,_]+/g; $$\ = "\n"; print for @parts;' omni/controller.py calcs/scripts/*.py | awk '{print $$2}')
+#---do not target arguments if using python
+.PHONY: banner ${RUN_ARGS}
+
+#---hack to always run makefile interfaced with python
+scripts = omni/controller.py
+$(shell touch $(scripts))
+checkfile = .pipeline_up_to_date
 
 #---collect arguments
 RUN_ARGS_UNFILTER := $(wordlist 1,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
 RUN_ARGS := $(filter-out banner help,$(RUN_ARGS_UNFILTER))
 $(eval $(RUN_ARGS):;@:)
+
+#---valid function names from the python script
+TARGETS := $(shell perl -n -e '@parts = /^def\s+[a-z,_]+/g; $$\ = "\n"; \
+print for @parts;' omni/controller.py calcs/scripts/*.py | awk '{print $$2}')
+
+#---default and arbitrary make targets
+default: $(checkfile)
+$(TARGETS): $(checkfile)
 
 #---exit if target not found
 controller_function = $(word 1,$(RUN_ARGS))
@@ -28,25 +40,14 @@ ifeq ($(filter $(controller_function),$(TARGETS)),)
 endif
 endif
 
-#---do not target arguments if using python
-.PHONY: banner ${RUN_ARGS}
-
-#---hack to always run makefile interfaced with python
-scripts = omni/controller.py
-$(shell touch $(scripts))
-checkfile = .pipeline_up_to_date
-
 #---targets
 $(checkfile): $(scripts)
 	touch $(checkfile)
 	@echo "[STATUS] calling: python omni/controller.py ${RUN_ARGS} ${MAKEFLAGS}"
 	@python omni/controller.py ${RUN_ARGS} ${MAKEFLAGS} && echo "[STATUS] bye" || { echo "[STATUS] fail"; }
 
-#---default and arbitrary make targets
-default: $(checkfile)
-$(TARGETS): $(checkfile)
-
 #---display the help
 help:
 	@echo -n "[STATUS] printing readme: "
 	tail -n +7 omni/readme.md
+
--- omni/base/gromacs.py
+++ omni/base/gromacs.py
@@ -21,90 +21,131 @@ gmx4paths = {
 	'genbox':'genbox',
 	'make_ndx':'make_ndx',
 	'genion':'genion',
+	'genconf':'genconf',
 	'trjconv':'trjconv',
-	'trjcat':'trjcat',
+	'tpbconv':'tpbconv',
+	'vmd':'vmd',
 	'gmxcheck':'gmxcheck',
-	'gmxdump':'gmxdump',
 	}
 
 gmx5paths = {
 	'grompp':'gmx grompp',
 	'mdrun':'gmx mdrun',
-	'pdb2gmx':'pdb2gmx',
-	'editconf':'editconf',
+	'pdb2gmx':'gmx pdb2gmx',
+	'editconf':'gmx editconf',
 	'genbox':'gmx solvate',
-	'make_ndx':'make_ndx',
+	'make_ndx':'gmx make_ndx',
 	'genion':'gmx genion',
 	'trjconv':'gmx trjconv',
-	'trjcat':'trjcat',
-	'gmxcheck':'gmxcheck',
-	'gmxdump':'gmxdump',
+	'genconf':'gmx genconf',
+	'tpbconv':'gmx convert-tpr',
+	'gmxcheck':'gmx check',
+	'vmd':'vmd',
 	}
 	
 #---SETTINGS
 #-------------------------------------------------------------------------------------------------------------
 
-#---load configuration
-config_raw = {}
-#---look upwards if making docs so no tracebacks
-prefix = '../../../' if re.match('.+\/docs\/build$',os.getcwd()) else ''
-if os.path.isfile(prefix+'./gromacs.py'): execfile(prefix+'./gromacs.py',config_raw)
-else: execfile(os.environ['HOME']+'/.automacs.py',config_raw)
-machine_configuration = config_raw['machine_configuration']
+def prepare_machine_configuration(hostname=None):
 
-#---select a machine configuration
-this_machine = 'LOCAL'
-hostnames = [key for key in machine_configuration 
-	if any([varname in os.environ and (
-	re.search(key,os.environ[varname])!=None or re.match(key,os.environ[varname]))
-	for varname in ['HOST','HOSTNAME']])]
-if len(hostnames)>1: raise Exception('[ERROR] multiple machine hostnames %s'%str(hostnames))
-elif len(hostnames)==1: this_machine = hostnames[0]
-else: this_machine = 'LOCAL'
-print '[STATUS] setting gmxpaths for machine: %s'%this_machine
-machine_configuration = machine_configuration[this_machine]
+	"""
+	Prepare the machine configuration according to $HOSTNAME or for a custom machine.
+	"""
 
-#---modules in LOCAL configuration must be loaded before checking version
-module_path = '/usr/share/Modules/default/init/python.py'
-if 'modules' in machine_configuration and machine_configuration['modules']:
-	print '[STATUS] found modules in LOCAL configuration'
-	if 'module_path' in machine_configuration: module_path = machine_configuration['module_path']
-	try: execfile(module_path)
-	except: raise Exception('could not execute %s'%module_path)
-	print '[STATUS] unloading GROMACS'
-	#---note that modules that rely on dynamically-linked C-code must use EnvironmentModules
-	modules = machine_configuration['modules']
-	modules = [modules] if type(modules)==str else modules
-	for mod in modules:
-		print '[STATUS] module load %s'%mod
-		module('load',mod)
-	if 'mod' in globals(): del mod
+	#---load configuration
+	config_raw = {}
+	#---look upwards if making docs so no tracebacks
+	prefix = '../../../' if re.match('.+\/docs\/build$',os.getcwd()) else ''
+	if os.path.isfile(prefix+'./config.py'): execfile(prefix+'./gromacs.py',config_raw)
+	else: execfile(os.environ['HOME']+'/.automacs.py',config_raw)
+	machine_configuration = config_raw['machine_configuration']
 
-#---basic check for gromacs version series
-suffix = '' if 'suffix' not in machine_configuration else machine_configuration['suffix']
-check_gmx = subprocess.Popen('gmx%s'%suffix,shell=True,
-	stdout=subprocess.PIPE,stderr=subprocess.PIPE).communicate()
-if not re.search('not found',check_gmx[1]): gmx_series = 5
-else:
-	check_mdrun = ' '.join(subprocess.Popen('mdrun%s -g /tmp/md.log'%suffix,
-		shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE).communicate())
-	if re.search('VERSION 4',check_mdrun): gmx_series = 4
-	else: raise Exception('gromacs is absent')
-	del check_mdrun
-print '[NOTE] using GROMACS %d'%gmx_series
+	#---select a machine configuration
+	this_machine = 'LOCAL'
+	if not hostname:
+		hostnames = [key for key in machine_configuration 
+			if any([varname in os.environ and (
+			re.search(key,os.environ[varname])!=None or re.match(key,os.environ[varname]))
+			for varname in ['HOST','HOSTNAME']])]
+	else: hostnames = [key for key in machine_configuration if re.search(key,hostname)]
+	if len(hostnames)>1: raise Exception('[ERROR] multiple machine hostnames %s'%str(hostnames))
+	elif len(hostnames)==1: this_machine = hostnames[0]
+	else: this_machine = 'LOCAL'
+	print '[STATUS] setting gmxpaths for machine: %s'%this_machine
+	machine_configuration = machine_configuration[this_machine]
+	#---compute total number of processors for the user if missing
+	if ('nnodes' in machine_configuration and 'ppn' in machine_configuration 
+		and not 'nprocs' in machine_configuration):
+		machine_configuration['nprocs'] = machine_configuration['nnodes']*machine_configuration['ppn']
+	return machine_configuration,this_machine
 
-#---select the right GROMACS utilities names
-if gmx_series == 4: gmxpaths = dict(gmx4paths)
-if gmx_series == 5: gmxpaths = dict(gmx5paths)
+def prepare_gmxpaths(machine_configuration,override=False,gmx_series=False):
 
-#---modify gmxpaths according to hardware configuration
-config = machine_configuration
-if 'nprocs' in config and config['nprocs'] != None: gmxpaths['mdrun'] += ' -nt %d'%config['nprocs']
-if 'gpu_flag' in config: gmxpaths['mdrun'] += ' -nb %s'%config['gpu_flag']
+	"""
+	Prepare the paths to GROMACS executables.
+	"""
 
-machine_name = str(this_machine)
-del config,this_machine,gmx5paths,gmx4paths,config_raw,module_path
-del check_gmx,gmx_series,hostnames
-if suffix != '': gmxpaths = dict([(key,val+suffix) for key,val in gmxpaths.items()])
-del suffix
+	#---basic check for gromacs version series
+	suffix = '' if 'suffix' not in machine_configuration else machine_configuration['suffix']
+	check_gmx = subprocess.Popen('gmx%s'%suffix,shell=True,executable='/bin/bash',
+		stdout=subprocess.PIPE,stderr=subprocess.PIPE).communicate()
+	if override and 'gmx_series' in machine_configuration: 
+		gmx_series = machine_configuration['gmx_series']
+	elif not gmx_series:
+		if not re.search('command not found',check_gmx[1]): gmx_series = 5
+		else:
+			check_mdrun = ' '.join(subprocess.Popen('mdrun%s -g /tmp/md.log'%suffix,shell=True,
+				executable='/bin/bash',stdout=subprocess.PIPE,stderr=subprocess.PIPE).communicate())
+			if re.search('VERSION 4',check_mdrun): gmx_series = 4
+			elif not override: raise Exception('gromacs is absent')
+			else: print '[NOTE] preparing gmxpaths with override'
+	else: print '[NOTE] using GROMACS %d'%gmx_series
 
+	#---select the right GROMACS utilities names
+	if gmx_series == 4: gmxpaths = dict(gmx4paths)
+	if gmx_series == 5: gmxpaths = dict(gmx5paths)
+
+	#---modify gmxpaths according to hardware configuration
+	config = machine_configuration
+	if suffix != '': 
+		if gmx_series == 5:
+			for key,val in gmxpaths.items():
+				gmxpaths[key] = re.sub('gmx ','gmx%s '%suffix,val)
+		else: gmxpaths = dict([(key,val+suffix) for key,val in gmxpaths.items()])
+	if 'nprocs' in config and config['nprocs'] != None: gmxpaths['mdrun'] += ' -nt %d'%config['nprocs']
+	#---use mdrun_command for quirky mpi-type mdrun calls on clusters
+	if 'mdrun_command' in machine_configuration: gmxpaths['mdrun'] = machine_configuration['mdrun_command']
+	#---if any utilities are keys in config we override it and then perform uppercase substitutions from config
+	utility_keys = [key for key in gmxpaths if key in config]
+	if any(utility_keys):
+		for name in utility_keys:
+			gmxpaths[name] = config[name]
+			for key,val in config.items(): gmxpaths[name] = re.sub(key.upper(),str(val),gmxpaths[name])
+		del name
+	#---even if mdrun is customized in config we treat the gpu flag separately
+	if 'gpu_flag' in config: gmxpaths['mdrun'] += ' -nb %s'%config['gpu_flag']	
+	return gmxpaths
+
+#---load machine configuration and gmxpaths into globals
+machine_configuration,this_machine = prepare_machine_configuration()
+#---load environment modules from python to setup GROMACS if necessary/desired
+try:
+	#---modules in LOCAL configuration must be loaded before checking version
+	module_path = '/usr/share/Modules/default/init/python.py'
+	if 'modules' in machine_configuration:
+		import importlib
+		print '[STATUS] found modules in %s configuration'%this_machine
+		if 'module_path' in machine_configuration: module_path = machine_configuration['module_path']
+		execfile(module_path)
+		#try: execfile(module_path)
+		#except: raise Exception('could not execute %s'%module_path)
+		print '[STATUS] unloading GROMACS'
+		#---note that modules that rely on dynamically-linked C-code must use EnvironmentModules
+		modlist = machine_configuration['modules']
+		if type(modlist)==str: modlist = modlist.split(',')
+		for mod in modlist:
+			print '[STATUS] module load %s'%mod
+			module('load',mod)
+		del mod
+except: print '[STATUS] failed to use importlib to load modules'
+gmxpaths = prepare_gmxpaths(machine_configuration)
